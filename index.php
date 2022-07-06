9f80366c666b7c6bae2612035ddc0a29635a05f0
batakid <riancode22@gmail.com>
2022-07-06 11:02:54 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
   </head>
   <body>
     <h1>Hello Word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, odit. Obcaecati aspernatur nostrum excepturi similique adipisci corrupti eligendi, doloribus iusto suscipit expedita consequatur culpa modi a ullam inventore quae provident.</p>
   </body>
 </html>
